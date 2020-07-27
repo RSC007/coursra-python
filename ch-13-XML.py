f49cdfd60c0662c16ad73d86087215953a4f1ea8
rushi <rushichitte1998@gmail.com>
2020-07-26 23:35:44 -0700

    web scrapping with xml

---
+++ ch-13-XML.py
@@ -0,0 +1,44 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/comments_704587.xml' # chnage this link to which url data you want fetch
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if address == 'done': break             # to exit the infinity loop type "done"
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+
+    data = uh.read()
+    print('Retrieved', len(data), 'characters')
+    print(data.decode())                # your whole data printed in string format as it is
+    tree = ET.fromstring(data)          # this statement parse your decoded string
+
+results = tree.findall('.//count')      # to find perticular tag count
+
+total = 0
+for item in results:
+    lat = item.text
+    total = total + int(lat)
+print(total)
+    
